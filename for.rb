d65be0f9fd0afa10bb220e0b9a7be5efbab95baa
Jessica Long <jessicalong627@gmail.com>
2018-07-03 01:29:32 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -1,11 +1,10 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+	for charm in checklist
+	puts "Wingardium Leviosa"
 end
 
 
 
-for shoe in shoe_count 
-	puts "I'm putting a pair of shoes away."
-end
+
